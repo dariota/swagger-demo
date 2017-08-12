0daef29c8b332ab479424c76240d557376dfb2dd
Dário Tavares Antunes <dario@ntun.es>
2017-08-12 11:34:47 +0100

    Add client for remote (API G) and use command line args

--- client/echo_client.rb
+++ client/echo_client.rb
@@ -1,8 +1,17 @@
 require 'swagger_client'
+require 'echo_api_remote'
 
 api_client = SwaggerClient::EchoServerApi.new
+remote_client = EchoApiRemote::EchoServerApi.new
 
-data, status_code, headers = api_client.what_did_i_say_with_http_info("Hello Swagger!")
+data, status_code, headers = api_client.what_did_i_say_with_http_info(ARGV[0])
 
-puts "I said: \"#{data.client_said}\""
-puts status_code
+puts "Local:"
+puts "\tI said: \"#{data.client_said}\""
+puts "\tStatus: #{status_code}"
+
+data, status_code, headers = remote_client.what_did_i_say_with_http_info(ARGV[0])
+
+puts "Remote:"
+puts "\tI said: \"#{data.client_said}\""
+puts "\tStatus: #{status_code}"
